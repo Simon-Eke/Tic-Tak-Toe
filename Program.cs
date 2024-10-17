0b1f54ecd3ac74433421249de3716ceb69d9359b
Simon Eke <simon3eke@gmail.com>
2024-10-17 12:56:47 +0200

    Started commenting a plan.

--- Program.cs
+++ Program.cs
@@ -12,6 +12,27 @@
 
             char[,] userChoiceBoard = CreateBoard.Create(gridSize, gridSize);
             DisplayBoard.Display(userChoiceBoard, gridSize);
-        }        
+            
+            /* MAIN GAME LOOP
+             
+             TODO - User interface?
+             TODO - Clear output in console after every move.
+             TODO - Thread.Sleep(~3000) när datorn "gör sitt drag"
+             
+             
+             
+             */
+            //while (true)
+            //{
+            //    Console.WriteLine("What's your next move? (1-9)");
+            //    bool validMove;
+            //    do
+            //    {
+            //        validMove = int.TryParse(Console.ReadLine(), out int userPositionChoice);
+            //        if (userPositionChoice)
+            //    } while (validMove);
+            //}
+
+        }
     }    
 }
